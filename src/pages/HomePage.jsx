955581f864ca971c142f2f26d5bfb3c936aeb4b0
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2023-11-14 23:04:27 +0700

    Add Section Just Arrived

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,14 @@
 import React from 'react'
 import Header from '../parts/Header'
 import Hero from '../parts/Hero'
+import JustArrived from '../parts/HomePage/JustArrived'
 
 const HomePage = () => {
   return (
     <>
       <Header />
       <Hero />
+      <JustArrived />
     </>
   )
 }
